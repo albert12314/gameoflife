e38d901890db022be285c018985804bdbf17da6d
alberthambardzumian <albert.h8.y@tumo.org>
2023-02-19 14:30:51 +0400

    added lesson 4

---
+++ Lesson4/script.js
@@ -0,0 +1,14 @@
+// var p = document.querySelectorAll('p');
+// console.log(p);
+
+// var myClass = document.querySelectorAll(".myClass");
+// console.log(myClass);
+
+// var id = document.querySelector("#yngerJan");
+// console.log(id);
+
+// var button = document.querySelector("button");
+
+// button.addEventListener('click',()=>{
+//     yngerJan.innerHTML = "sfgvueryg";
+// })
---
+++ Lesson4/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server)
+
+var messages = [];
+app.use(express.static('.'))
+
+app.get('/',(req,res)=>{
+    res.redirect('index.html')
+})
+
+io.on("connection", (socket)=>{
+    for(var i in messages){
+        socket.emit("display message", message[i]);
+    }
+    socket.on("send message", (data) =>{
+        messages.push(data);
+        io.sockets.emit("display message", data)
+    });
+});
+
+app.listen(3000)
